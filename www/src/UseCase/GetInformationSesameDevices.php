e416ef5e6dcce5db3de85b43a851891b523f3680
bell.tanaka <dev@shigeki.tokyo>
2021-07-30 12:24:11 +0900

    [update] apply formatter.

--- www/src/UseCase/GetInformationSesameDevices.php
+++ www/src/UseCase/GetInformationSesameDevices.php
@@ -4,15 +4,15 @@ declare(strict_types=1);
 
 namespace Fer\Deli\UseCase;
 
+use Fer\Deli\Service\Formatter\SesameDeviceFormatterInterface;
 use Fer\Deli\Service\SmartLock\Actor\Room;
 use Fer\Deli\Service\SmartLock\Status;
-
-use function number_format;
 
 class GetInformationSesameDevices
 {
     public function __construct(
-        private Status $status
+        private Status $status,
+        private SesameDeviceFormatterInterface $formatter
     ) {
     }
 
@@ -24,13 +24,7 @@ class GetInformationSesameDevices
         $devices = [];
         foreach (Room::values() as $room) {
             $deviceInfo = $this->status->run($room);
-
-            $devices[$room->getValue()] = [
-                'room_name' => $room->getName(),
-                'state' => $deviceInfo['CHSesame2Status'],
-                'battery_level' => $deviceInfo['batteryPercentage'] . '%',
-                'battery_voltage' => number_format($deviceInfo['batteryVoltage'], 2) . 'V',
-            ];
+            $devices[$room->getValue()] = $this->formatter->format($room, $deviceInfo);
         }
 
         return $devices;
